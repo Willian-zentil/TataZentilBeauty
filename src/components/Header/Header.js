cf64054c43fc87d4aa4d9af21d4bea84045a2fb3
willianZentil <willian.zentil@maeztra.com>
2023-08-03 20:45:10 -0300

    fix#safari-video

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,21 @@ import logoHeader from '../../images/logomarca_fundo_transparente.png'
 import video from '../../images/video.mp4'
 
 const Header = () => {
-
   useEffect(() => {
-    const play_video = setTimeout(() => {
-      document.getElementById('vid').src('../../images/video.mp4');
-    }, 1500);
-    
+
+    const play_video = () => {
+      setTimeout(() => {
+        document.getElementById('vid').src = video;
+      }, 1500);
+    }
+
+    if (document.readyState === 'complete') {
+      play_video()
+    } else {
+      window.addEventListener('load', play_video());
+      return () => window.removeEventListener('load', play_video());
+    }
+
     return () => {
       clearTimeout(play_video)
     }
@@ -19,7 +28,7 @@ const Header = () => {
 
   return (
     <header id='header' className={styles.header}>
-      <video id='vid' muted loop={true} autostart={true} autoPlay={true} playsinline={true} src={video} className={styles.video} type={"video/mp4"} />
+      <video id='vid' muted loop={true} autostart={true} autoPlay={true} playsInline={true} src={video} className={styles.video} type={"video/mp4"} />
       <div className={styles.menu}>
         <div className={styles.container}>
           <a className={styles.logoLink} href='#header'><img src={logoHeader} alt='TataZentil Beauty' className={styles.logo} /></a>
