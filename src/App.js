d3cbdbd48786e9c57b9242bbf5fd1ca7f571b308
willianZentil <willian.zentil@maeztra.com>
2023-11-09 18:31:29 -0300

    fix#estrutura-seo

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
   return (
     <Fragment>
       <Header>
+        <Extensao />
         <MiniBanners />
         <TipoTecnicas />
-        <Extensao />
         <RatingClients />
         <Salao />
       </Header>
